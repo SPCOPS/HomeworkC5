c7c1834f3b44dc104090bdb573ed01d7cae556c8
SPECTR <ivanov8465@mail.ru>
2023-03-25 00:11:08 +0300

    Сделал 2 задание

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 10);
+}
+int summa(int[] array)
+{
+int sum = 0;
+int i = 1;
+while (i < array.Length)
+{
+sum = sum + array[i];
+i = i + 2;
+}
+return sum;
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[a];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.Write($"Cумма элементов, стоящих на нечётных позициях: {summa(array)}");
